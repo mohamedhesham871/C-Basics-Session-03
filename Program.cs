dc6e760a3907367acfc664c1dd4872b294129eb7
20100 <20100@MOHAMED-HESHAM>
2024-12-11 20:08:01 +0200

    Q2 Answer

--- Program.cs
+++ Program.cs
@@ -5,16 +5,31 @@
         static void Main(string[] args)
         {
             #region Q1 : Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no
-            Console.WriteLine("enter number :");
-            int x=int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter number :");
+            //int x=int.Parse(Console.ReadLine());
 
-            if(x%3==0 &&  x%4==0)
+            //if(x%3==0 &&  x%4==0)
+            //{
+            //    Console.WriteLine("YES");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NO");
+            //}
+            #endregion
+
+            #region Q2: Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+
+            Console.WriteLine("enter number :");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x >=0)
             {
-                Console.WriteLine("YES");
+                Console.WriteLine("positive");
             }
             else
             {
-                Console.WriteLine("NO");
+                Console.WriteLine("negative");
             }
             #endregion
         }
