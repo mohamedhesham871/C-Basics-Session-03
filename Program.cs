7e399be8ef8500449c9829114a9232a20eed38dc
20100 <20100@MOHAMED-HESHAM>
2024-12-11 19:56:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Basics_Session_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
